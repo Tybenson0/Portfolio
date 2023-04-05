e323b56276190c7cd65162e7e34112646556426b
Tybenson0 <105068044+Tybenson0@users.noreply.github.com>
2023-04-04 18:57:09 -0700

    space-ship animations completed

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ $(document).ready(function(){
         $(".shuttle").addClass("shoot-off");
       });
     
+
+      $('.waitBeforeNavigate').on("click", function (ev) {
+        ev.preventDefault();                // prevent default anchor behavior
+        const goTo = $(this).attr("href");  // store anchor href
+             
+        // do something while timeOut ticks ... 
+             
+        setTimeout(function(){
+          window.location = goTo;
+        }, 300);                           // time in ms
+      }); 
 });
 
 const swup  = new Swup()
+
 
