7e31f41d5ef8e400789ba0f5bc82c77c61c186ad
Tybenson0 <105068044+Tybenson0@users.noreply.github.com>
2023-04-09 13:44:21 -0700

    additions to dev-debrief, and projects page.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+      //below is the click functions for rocket animations
       $(".nav-item").click(function(){
           $(".shuttle").addClass("shoot-off");
         });
@@ -11,9 +13,23 @@ $(document).ready(function(){
       $(".station").click(function(){
           $(".station").addClass("shoot-off-back");
         });
+
+     
+        $('#project-1').on('click', function() {
+          if ($('#project-1').css('opacity') == .88) $('#project-1').css('opacity', 0);
+          else $('#project-1').css('opacity', .88);
+        });
+        $('#project-2').on('click', function() {
+          if ($('#project-2').css('opacity') == .88) $('#project-2').css('opacity', 0);
+          else $('#project-2').css('opacity', .88);
+        });
+        $('#project-3').on('click', function() {
+          if ($('#project-3').css('opacity') == .88) $('#project-3').css('opacity', 0);
+          else $('#project-3').css('opacity', .88);
+        });
       
     
-
+      //below adds a slight delay to page load to allow for rocket animation to complete
       $('.waitBeforeNavigate').on("click", function (ev) {
         ev.preventDefault();                // prevent default anchor behavior
         const goTo = $(this).attr("href");  // store anchor href
